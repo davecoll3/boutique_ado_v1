9229e0c61f96998ed490754bf7fa33daa6f54ec8
Daithí <davecoll3@gmail.com>
2023-04-11 09:42:22 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MvdFYLixfc2GbbgADbPhdcyTW6FZHEkNFWY3TkXNsgw6CHCiGFl39EEP29ywYMmcZWnfFtmzfJt6gPiCqvlICTJ00yHNdeQBz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
