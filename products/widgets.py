6bb7d1ba59894970cb3081bb8b506c0a3ba031cf
Daithí <davecoll3@gmail.com>
2023-04-14 15:45:01 +0000

    Refactored code in products app

--- products/widgets.py
+++ products/widgets.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
-    input_text = ('')
-    template_name = 'products/custon_widget_template/custom_clearable_file_input.html'
+    input_text = _('')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
