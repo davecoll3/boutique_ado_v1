b8074e5d38a66ed7245b80a39e628fe31d4513f0
Daithí <davecoll3@gmail.com>
2023-04-14 17:03:39 +0000

    Refactored settings.py code for MEDIA_ROOT - back to origional

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,7 +178,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
